a27d52815b72f9e4711856a46af80dced3f58a35
EIndriksons <edgars@indriksons.lv>
2020-11-01 13:29:35 +0200

    useState - useState with object & spread operator

--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,7 +1,30 @@
 import React, { useState } from 'react';
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  // * with arrays you can easily filter and re-construct them
+  // * but with objects it is a little bit different, because you want to keep some information
+
+  const [person, setPerson] = useState({
+    name: 'peter',
+    age: 24,
+    message: 'random message form peter',
+  });
+
+  const changeMessage = () => {
+    // * solution is spread operator ...
+    setPerson({ ...person, message: 'hello world' });
+  };
+
+  return (
+    <>
+      <h3>{person.name}</h3>
+      <h3>{person.age}</h3>
+      <h3>{person.message}</h3>
+      <button className="btn" onClick={changeMessage}>
+        Change Message
+      </button>
+    </>
+  );
 };
 
 export default UseStateObject;
