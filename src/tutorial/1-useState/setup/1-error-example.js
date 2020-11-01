48127c53c5f5d728f2bed02af22c00d8d2e54aa6
EIndriksons <edgars@indriksons.lv>
2020-11-01 12:40:29 +0200

    useState - useState baics - intro

--- src/tutorial/1-useState/setup/1-error-example.js
+++ src/tutorial/1-useState/setup/1-error-example.js
@@ -1,7 +1,20 @@
 import React from 'react';
 
 const ErrorExample = () => {
-  return <h2>useState error example</h2>;
+  // Not gonna work because it does not trigger re-rendering
+  let title = 'random title';
+  const handleClick = () => {
+    title = 'hello people';
+  };
+
+  return (
+    <React.Fragment>
+      <h2>{title}</h2>
+      <button type="button" className="btn" onClick={handleClick}>
+        Change Title
+      </button>
+    </React.Fragment>
+  );
 };
 
 export default ErrorExample;
