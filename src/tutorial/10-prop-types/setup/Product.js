24a709e9a3f7961a1d241283e1002402d32b6220
EIndriksons <edgars@indriksons.lv>
2020-11-03 18:20:56 +0200

    PropTypes - defaultProps

--- src/tutorial/10-prop-types/setup/Product.js
+++ src/tutorial/10-prop-types/setup/Product.js
@@ -1,23 +1,36 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import defaultImage from '../../../assets/default-image.jpeg';
 
 // * PropTypes are necessary because if the component gets transferred
 // * nonexistant value - it will cause an error
 
 const Product = (props) => {
+  // * two ways how to use default props
+  // * 1) use Product.defaultProps
+  // * 2) put {props.name || 'default name'} in each output
+
+  const url = props.image && props.image.url;
+
   return (
     <article className="product">
-      <img src={props.image.url} alt={props.name} />
+      <img src={url || defaultImage} alt={props.name} />
       <h4>{props.name}</h4>
-      <p>${props.price}</p>
+      <p>${props.price || 3.99}</p>
     </article>
   );
 };
 
 Product.propTypes = {
-  image: PropTypes.object.isRequired,
   name: PropTypes.string.isRequired,
   price: PropTypes.number.isRequired,
+  image: PropTypes.object.isRequired,
 };
+
+// Product.defaultProps = {
+//   name: 'Default Name',
+//   price: 3.99,
+//   image: defaultImage,
+// };
 
 export default Product;
