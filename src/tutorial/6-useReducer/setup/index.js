0c5cf87682caada373b29930f74b53496d3a0ce0
EIndriksons <edgars@indriksons.lv>
2020-11-03 14:39:01 +0200

    useReducer - adding Items

--- src/tutorial/6-useReducer/setup/index.js
+++ src/tutorial/6-useReducer/setup/index.js
@@ -3,32 +3,62 @@ import Modal from './Modal';
 import { data } from '../../../data';
 // reducer function
 
+// * reducer is similar to Redux
+// * its a single central place to put global app state
+// * and dispatch changes to state from various parts of the app
+
+const reducer = (state, action) => {
+  if (action.type === 'ADD_ITEM') {
+    const newPeople = [...state.people, action.payload];
+    return {
+      ...state,
+      people: newPeople,
+      isModalOpen: true,
+      modalContent: 'Item Added',
+    };
+  }
+  if (action.type === 'NO_VALUE') {
+    return {
+      ...state,
+      isModalOpen: true,
+      modalContent: 'Please Enter Value',
+    };
+  }
+
+  return state;
+};
+
+const defaultState = {
+  people: [],
+  isModalOpen: false,
+  modalContent: '',
+};
+
 const Index = () => {
   const [name, setName] = useState('');
-  const [people, setPeople] = useState(data);
-  const [showModal, setShowModal] = useState(false);
+  const [state, dispatch] = useReducer(reducer, defaultState);
 
   const handleSubmit = (e) => {
     e.preventDefault();
     if (name) {
-      setShowModal(true);
-      setPeople([...people, { id: new Date().getTime().toString(), name }]);
+      const newItem = { id: new Date().getTime().toString(), name };
+      dispatch({ type: 'ADD_ITEM', payload: newItem });
       setName('');
     } else {
-      setShowModal(true);
+      dispatch({ type: 'NO_VALUE' });
     }
   };
 
   return (
     <>
-      {showModal && <Modal />}
+      {state.isModalOpen && <Modal modalContent={state.modalContent} />}
       <form onSubmit={handleSubmit} className="form">
         <div>
           <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
         </div>
         <button type="submit">Add Person</button>
       </form>
-      {people.map((person) => {
+      {state.people.map((person) => {
         return (
           <div key={person.id}>
             <h4>{person.name}</h4>
