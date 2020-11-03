11e87abbb4519fe9f6ad4a4dea729346b34d7867
EIndriksons <edgars@indriksons.lv>
2020-11-03 18:45:08 +0200

    React Router - Links

--- src/tutorial/11-react-router/setup/index.js
+++ src/tutorial/11-react-router/setup/index.js
@@ -15,18 +15,26 @@ import Navbar from './Navbar';
 const ReactRouterSetup = () => {
   // ! react Router goes down the list and displays whichever
   // ! component is in its path and matches the path
+  // * <Switch> component displays only the first match
+  // * <Error> component with path="*" selects any other path
 
   return (
     <Router>
-      <Route exact path="/">
-        <Home />
-      </Route>
-      <Route path="/about">
-        <About />
-      </Route>
-      <Route path="/people">
-        <People />
-      </Route>
+      <Navbar />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/people">
+          <People />
+        </Route>
+        <Route path="*">
+          <Error />
+        </Route>
+      </Switch>
     </Router>
   );
 };
