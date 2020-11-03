eea60b44ed1b1d0008a35c7fa30bdc045289cc54
EIndriksons <edgars@indriksons.lv>
2020-11-03 18:14:02 +0200

    PropTypes

--- src/tutorial/10-prop-types/setup/Product.js
+++ src/tutorial/10-prop-types/setup/Product.js
@@ -1,7 +1,23 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const Product = () => {
-  return <article className='product'>single product</article>;
+// * PropTypes are necessary because if the component gets transferred
+// * nonexistant value - it will cause an error
+
+const Product = (props) => {
+  return (
+    <article className="product">
+      <img src={props.image.url} alt={props.name} />
+      <h4>{props.name}</h4>
+      <p>${props.price}</p>
+    </article>
+  );
+};
+
+Product.propTypes = {
+  image: PropTypes.object.isRequired,
+  name: PropTypes.string.isRequired,
+  price: PropTypes.number.isRequired,
 };
 
 export default Product;
