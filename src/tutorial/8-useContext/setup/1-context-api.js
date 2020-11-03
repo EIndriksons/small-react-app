9a8eeb894cd9e3755ff817c8055e2570069be802
EIndriksons <edgars@indriksons.lv>
2020-11-03 15:33:02 +0200

    useContext & Context API

--- src/tutorial/8-useContext/setup/1-context-api.js
+++ src/tutorial/8-useContext/setup/1-context-api.js
@@ -3,6 +3,10 @@ import { data } from '../../../data';
 // more components
 // fix - context api, redux (for more complex cases)
 
+// * to use context API we must 1) initialize the Context
+const PersonContext = React.createContext();
+// provides two components - Provider, Consumer
+
 const ContextAPI = () => {
   const [people, setPeople] = useState(data);
   const removePerson = (id) => {
@@ -11,32 +15,33 @@ const ContextAPI = () => {
     });
   };
   return (
-    <>
+    // * 2) we then wrap the components in the Context Provider
+    // * 3) pass the necessary props through value
+    <PersonContext.Provider value={{ removePerson, people }}>
       <h3>prop drilling</h3>
-      <List people={people} removePerson={removePerson} />
-    </>
+      <List />
+    </PersonContext.Provider>
   );
 };
 
-const List = ({ people, removePerson }) => {
+const List = () => {
+  const data = useContext(PersonContext);
   return (
     <>
-      {people.map((person) => {
-        return (
-          <SinglePerson
-            key={person.id}
-            {...person}
-            removePerson={removePerson}
-          />
-        );
+      {data.people.map((person) => {
+        return <SinglePerson key={person.id} {...person} />;
       })}
     </>
   );
 };
 
-const SinglePerson = ({ id, name, removePerson }) => {
+const SinglePerson = ({ id, name }) => {
+  // * 4) access the passed props anywhere in the child components
+  // * by using useContext(ContextAPIName)
+  const { removePerson } = useContext(PersonContext);
+
   return (
-    <div className='item'>
+    <div className="item">
       <h4>{name}</h4>
       <button onClick={() => removePerson(id)}>remove</button>
     </div>
